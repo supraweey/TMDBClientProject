92ac22789f5b6b9e85eae1744cb67d75b3338406
FonSuprawee <suprawee.yimnium@accenture.com>
2023-08-24 23:31:46 +0700

    Separate notification and delete unused code

--- app/src/main/java/com/android/supraweey/tmdbclient/ui/NotificationActivity.kt
+++ app/src/main/java/com/android/supraweey/tmdbclient/ui/NotificationActivity.kt
@@ -2,20 +2,20 @@ package com.android.supraweey.tmdbclient.ui
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.AttributeSet
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.android.supraweey.tmdbclient.R
 import com.android.supraweey.tmdbclient.databinding.ActivityStartScreenBinding
+import kotlin.random.Random
 
-class StartScreenActivity : AppCompatActivity() {
+class NotificationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityStartScreenBinding
     private val CHANNEL_ID = "ChannelID"
@@ -27,32 +27,37 @@ class StartScreenActivity : AppCompatActivity() {
         binding = ActivityStartScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnHome.setOnClickListener {
-            PopularActivity.startActivity(this)
-        }
         binding.btnNotification.setOnClickListener {
             setNotification()
         }
     }
 
-    private fun setNotification(){
+    private fun setNotification() {
         createNotificationChannel()
 
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+        val intent = Intent(this, NotificationActivity::class.java)
+        val notificationOrderId = Random.nextInt(0, 4999)
+        val pendingIntent = getPendingIntent(intent, notificationOrderId)
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setContentTitle("Awesome")
             .setContentText("This is the content text")
             .setSmallIcon(R.drawable.ic_star)
             .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
             .build()
 
         val notificationManager = NotificationManagerCompat.from(this)
-        notificationManager.notify(NOTIFICATION_ID, notification)
+        notificationManager.notify(NOTIFICATION_ID, builder)
     }
 
-    private fun createNotificationChannel(){
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.O)
-        {
-            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH).apply {
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
                 lightColor = Color.GREEN
                 enableLights(true)
                 setShowBadge(true)
@@ -62,4 +67,13 @@ class StartScreenActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun getPendingIntent(intent: Intent, notificationID: Int): PendingIntent =
+        PendingIntent.getActivity(
+            applicationContext,
+            notificationID,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
 }
